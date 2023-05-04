1475ec81b948ebafd32bed987860288a79bc77b1
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-05-05 02:15:54 +0300

    Add title to add book form

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,6 +1,7 @@
 function AddBook() {
   return (
     <form className="add-book-form">
+      <h2>ADD NEW BOOK</h2>
       <input type="text" name="book-name" id="book-name" placeholder="Book title" />
       <input type="text" name="book-author" id="book-author" placeholder="Author" />
       <button type="submit">Add Book</button>
